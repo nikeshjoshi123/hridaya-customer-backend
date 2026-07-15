e695b9afd3703a0f38b8300f26421d406f5b3983
Nikesh Joshi <nikesh.joshi@office.mypay.com.np>
2026-07-15 12:52:35 +0545

    send otp un comment

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -9,19 +9,18 @@ const sendOtpEmail = async (email, otp) => {
   //     pass: process.env.EMAIL_PASS
   //   }
   // });
-  const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 456,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  tls:{
+    rejectUnauthorized:false
+  }
 });
-
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to: email,
